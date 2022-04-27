55dd253f3a061e7ba793371367c567b765569952
Shashwat <10794178+TheTrio@users.noreply.github.com>
2022-04-27 16:41:39 +0530

    packages

--- scripts/update_packages.sh
+++ scripts/update_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pacman -Qqen > /home/shashwat/stow/packages/packages.txt
 pacman -Qqem > /home/shashwat/stow/packages/aur_packages.txt
-flatpak list | cut -f 2 > /home/shashwat/stow/packages/flatpak_packages.txt
+flatpak list | cut -f 2 | grep -vi platform > /home/shashwat/stow/packages/flatpak_packages.txt
