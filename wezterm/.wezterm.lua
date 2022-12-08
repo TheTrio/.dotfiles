05aa304f6b1258fafba69c3bcc500eafaed23a9f
Shashwat <10794178+TheTrio@users.noreply.github.com>
2022-12-08 22:03:36 +0530

    add keybinds for copying on select

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,47 +1,55 @@
 local wezterm = require 'wezterm'
 
-return {
-    font = wezterm.font('JetBrains Mono'),
-    default_prog = { '/usr/bin/fish', '-l' },
+function make_mouse_binding(dir, streak, button, mods, action)
+  return {
+    event = { [dir] = { streak = streak, button = button } },
+    mods = mods,
+    action = action,
+  }
+end
 
-    colors = {
-        cursor_bg = '#E84A72',
-        foreground = '#BABABA',
-        background = '#16161C',
-        ansi = {
-            '#131419',
-            '#E95678',
-            '#29D398',
-            '#FAB795',
-            '#26BBD9',
-            '#EE64AE',
-            '#59E3E3',
-            '#C7C7C7',
-        },
-        brights = {
-            '#676767',
-            '#EC6A88',
-            '#3FDAA4',
-            '#FBC3A7',
-            '#3FC6DE',
-            '#F075B7',
-            '#6BE6E6',
-            '#FEFFFF',
-        },
+return {
+  font = wezterm.font('JetBrains Mono'),
+  default_prog = { '/usr/bin/fish', '-l' },
+
+  colors = {
+    cursor_bg = '#E84A72',
+    foreground = '#BABABA',
+    background = '#16161C',
+    ansi = {
+      '#131419',
+      '#E95678',
+      '#29D398',
+      '#FAB795',
+      '#26BBD9',
+      '#EE64AE',
+      '#59E3E3',
+      '#C7C7C7',
     },
-    hide_tab_bar_if_only_one_tab = true,
-    window_background_opacity = 0.85,
-    animation_fps = 20,
-    default_cursor_style = 'SteadyBlock',
-    enable_wayland = true,
-    keys = {
-        {
-            key = 'n',
-            mods = 'SHIFT|CTRL',
-            action = wezterm.action.SpawnWindow
-        },
+    brights = {
+      '#676767',
+      '#EC6A88',
+      '#3FDAA4',
+      '#FBC3A7',
+      '#3FC6DE',
+      '#F075B7',
+      '#6BE6E6',
+      '#FEFFFF',
     },
-    hyperlink_rules = {
+  },
+  hide_tab_bar_if_only_one_tab = true,
+  window_background_opacity = 0.85,
+  animation_fps = 20,
+  default_cursor_style = 'SteadyBlock',
+  enable_wayland = true,
+  keys = {
+    {
+      key = 'n',
+      mods = 'SHIFT|CTRL',
+      action = wezterm.action.SpawnWindow
+    },
+  },
+  hyperlink_rules = {
     -- Linkify things that look like URLs and the host has a TLD name.
     -- Compiled-in default. Used if you don't specify any hyperlink_rules.
     {
@@ -86,5 +94,16 @@ return {
       regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
       format = 'https://www.github.com/$1/$3',
     },
+  },
+  mouse_bindings = {
+    make_mouse_binding('Up', 1, 'Left', 'NONE',
+      wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+    make_mouse_binding('Up', 1, 'Left', 'SHIFT',
+      wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+    make_mouse_binding('Up', 1, 'Left', 'ALT', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
+    make_mouse_binding('Up', 1, 'Left', 'SHIFT|ALT',
+      wezterm.action.CompleteSelectionOrOpenLinkAtMouseCursor 'ClipboardAndPrimarySelection'),
+    make_mouse_binding('Up', 2, 'Left', 'NONE', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
+    make_mouse_binding('Up', 3, 'Left', 'NONE', wezterm.action.CompleteSelection 'ClipboardAndPrimarySelection'),
   },
 }
